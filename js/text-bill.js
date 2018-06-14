79cc345edd8c12b7ddc430b5d6939ded1c4e0c88
Yegan <yeganj@gmail.com>
2018-06-14 10:53:32 +0200

    HandleBars.js test

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,10 +1,11 @@
 var billTypeTextElem = document.querySelector(".billTypeText")
 var addToBillBtnElem = document.querySelector(".addToBillBtn")
 var totalOneElem = document.querySelector(".totalOne")
-var callsTotalElem = document.querySelector(".callTotalOne")
-var smsTotalElem = document.querySelector(".smsTotalOne")
-
-
+// var callsTotalElem = document.querySelector(".callTotalOne")
+// var smsTotalElem = document.querySelector(".smsTotalOne")
+var userTemplate = document.querySelector(".userTotals").innerHTML;
+var dataOutput = document.querySelector(".handleBarsDisplayInputBill")
+var templateSource = Handlebars.compile(userTemplate)
 
 var myFactoryFunction = TextBillFactory()
 
@@ -14,9 +15,12 @@ function textDisplay() {
 
   myFactoryFunction.billCalculate(billTypeEntered)
 
-  callsTotalElem.innerHTML = myFactoryFunction.call().toFixed(2);
-  smsTotalElem.innerHTML = myFactoryFunction.sms().toFixed(2);
-  totalOneElem.innerHTML = myFactoryFunction.total().toFixed(2);
+  var htmlTemplate = templateSource(billTypeEntered);
+
+  dataOutput.innerHTML = htmlTemplate;
+  // callsTotalElem.innerHTML = myFactoryFunction.call().toFixed(2);
+  // smsTotalElem.innerHTML = myFactoryFunction.sms().toFixed(2);
+  // totalOneElem.innerHTML = myFactoryFunction.total().toFixed(2);
 
 
 
